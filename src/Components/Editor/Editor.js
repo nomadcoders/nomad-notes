563445377f7816fad3904bada9e49fa3bfd0a16f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-12 19:27:45 +0900

    #1.11 Add Note Route

---
+++ src/Components/Editor/Editor.js
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+import MarkdownRenderer from "react-markdown-renderer";
+import TextareaAutosize from "react-textarea-autosize";
+
+const TitleInput = styled(TextareaAutosize)`
+  font-size: 50px;
+  font-weight: 600;
+  width: 100%;
+  &::placeholder {
+    font-weight: 600;
+  }
+`;
+
+const ContentPreview = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 50px;
+`;
+
+const ContentInput = styled(TextareaAutosize)`
+  font-size: 18px;
+  margin-top: 15px;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 50px;
+`;
+
+const Button = styled.button``;
+
+export default class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: props.title || "",
+      content: props.content || "",
+      id: props.id || null
+    };
+  }
+  render() {
+    const { title, content } = this.state;
+    return (
+      <>
+        <TitleContainer>
+          <TitleInput
+            value={title}
+            onChange={this._onInputChange}
+            placeholder={"Untitled..."}
+            name={"title"}
+          />
+          <Button onClick={this._onSave}>Save</Button>
+        </TitleContainer>
+        <ContentPreview>
+          <ContentInput
+            value={content}
+            onChange={this._onInputChange}
+            placeholder={"# This supports markdown!"}
+            name={"content"}
+          />
+          <MarkdownRenderer markdown={content} className={"markdown"} />
+        </ContentPreview>
+      </>
+    );
+  }
+  _onInputChange = event => {
+    const {
+      target: { value, name }
+    } = event;
+    this.setState({
+      [name]: value
+    });
+  };
+  _onSave = () => {
+    const { onSave } = this.props;
+    const { title, content, id } = this.state;
+    onSave(title, content, id);
+  };
+}
--- src/Routes/Add/Add.js
+++ src/Routes/Add/Add.js
@@ -1,6 +1,34 @@
 import React from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Editor from "../../Components/Editor";
+
+const ADD_NOTE = gql`
+  mutation createNote($title: String!, $content: String!) @client {
+    createNote(title: $title, content: $content) {
+      id
+    }
+  }
+`;
+
 export default class Add extends React.Component {
   render() {
-    return "hi";
+    return (
+      <Mutation mutation={ADD_NOTE}>
+        {createNote => {
+          this.createNote = createNote;
+          return <Editor onSave={this._onSave} />;
+        }}
+      </Mutation>
+    );
   }
+  _onSave = (title, content) => {
+    const {
+      history: { push }
+    } = this.props;
+    if (title !== "" && content !== "") {
+      this.createNote({ variables: { title, content } });
+      push("/");
+    }
+  };
 }
