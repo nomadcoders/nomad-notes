b03de1c1d57d288813257cfc196821a57c0aaec3
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-12 18:11:25 +0900

    #1.4 Note Query part One

--- src/clientState.js
+++ src/clientState.js
@@ -1,5 +1,12 @@
 export const defaults = {
-  notes: []
+  notes: [
+    {
+      __typename: "Note",
+      id: 1,
+      title: "First",
+      content: "Second"
+    }
+  ]
 };
 export const typeDefs = [
   `
@@ -12,8 +19,8 @@ export const typeDefs = [
         note(id: Int!): Note
     }
     type Mutation{
-        createNote(title: String!, content: String!)
-        editNote(id: String!, title: String!, content:String!)
+        createNote(title: String!, content: String!): Note
+        editNote(id: String!, title: String!, content:String!): Note
     }
     type Note{
         id: Int!
@@ -24,6 +31,10 @@ export const typeDefs = [
 ];
 export const resolvers = {
   Query: {
-    notes: () => true
+    note: (_, variables, { getCacheKey }) => {
+      const id = getCacheKey({ __typename: "Note", id: variables.id });
+      console.log(id);
+      return null;
+    }
   }
 };
