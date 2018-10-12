c588cabeec6f193a89177e693eab688bdafa3cc6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-12 17:01:23 +0900

    #1.3 Type Definitions for Offline Schema

--- src/clientState.js
+++ src/clientState.js
@@ -1,3 +1,29 @@
-export const defaults = {};
-export const resolvers = {};
-export const typeDefs = {};
+export const defaults = {
+  notes: []
+};
+export const typeDefs = [
+  `
+    schema {
+        query: Query
+        mutation: Mutation
+    }
+    type Query {
+        notes: [Note]!
+        note(id: Int!): Note
+    }
+    type Mutation{
+        createNote(title: String!, content: String!)
+        editNote(id: String!, title: String!, content:String!)
+    }
+    type Note{
+        id: Int!
+        title: String!
+        content: String!
+    }
+    `
+];
+export const resolvers = {
+  Query: {
+    notes: () => true
+  }
+};
