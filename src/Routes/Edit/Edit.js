dc38956b2e8a2667392e2dccc4a6ea6d892ed2ad
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-12 19:37:26 +0900

    #1.12 Edit Note Route

--- src/Routes/Edit/Edit.js
+++ src/Routes/Edit/Edit.js
@@ -1,6 +1,53 @@
 import React from "react";
+import { Query, Mutation } from "react-apollo";
+import { GET_NOTE } from "../../queries";
+import Editor from "../../Components/Editor";
+import gql from "graphql-tag";
+
+export const EDIT_NOTE = gql`
+  mutation editNote($id: Int!, $title: String!, $content: String!) @client {
+    editNote(id: $id, title: $title, content: $content) {
+      id
+    }
+  }
+`;
+
 export default class Edit extends React.Component {
   render() {
-    return "hi";
+    const {
+      match: {
+        params: { id }
+      }
+    } = this.props;
+    return (
+      <Query query={GET_NOTE} variables={{ id }}>
+        {({ data }) =>
+          data.note ? (
+            <Mutation mutation={EDIT_NOTE}>
+              {editNote => {
+                this.editNote = editNote;
+                return (
+                  <Editor
+                    title={data.note.title}
+                    content={data.note.content}
+                    id={data.note.id}
+                    onSave={this._onSave}
+                  />
+                );
+              }}
+            </Mutation>
+          ) : null
+        }
+      </Query>
+    );
   }
+  _onSave = (title, content, id) => {
+    const {
+      history: { push }
+    } = this.props;
+    if (title !== "" && content !== "" && id) {
+      this.editNote({ variables: { title, content, id } });
+      push("/");
+    }
+  };
 }
