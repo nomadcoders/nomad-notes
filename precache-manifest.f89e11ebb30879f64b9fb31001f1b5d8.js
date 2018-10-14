b7c77bc0760446e796c874325783181a95b67716
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-14 18:56:10 +0900

    Updates

---
+++ precache-manifest.f89e11ebb30879f64b9fb31001f1b5d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "729364f77d71e9b09e7e956dfad5e98f",
+    "url": "/nomad-notes/static/media/plus.729364f7.svg"
+  },
+  {
+    "revision": "3ba8009f59e9f0a8ef0b",
+    "url": "/nomad-notes/static/js/runtime~main.3ba8009f.js"
+  },
+  {
+    "revision": "383e0eaa2a751ed3f679",
+    "url": "/nomad-notes/static/js/main.383e0eaa.chunk.js"
+  },
+  {
+    "revision": "49af322c6558c92edbac",
+    "url": "/nomad-notes/static/js/1.49af322c.chunk.js"
+  },
+  {
+    "revision": "28dc5291e6b65e28d411c8a43334cc39",
+    "url": "/nomad-notes/index.html"
+  }
+];
